88be969d5517803929d9cc3fd1f16984044a7221
ralph14 <ralphr@packtpub.com>
2017-08-31 10:17:07 +0530

    Add files via upload

---
+++ Chapter01/beosbank-forge-demo/src/main/java/com/beosbank/forge/demo/HelloJBossWorld.java
@@ -0,0 +1,28 @@
+package com.beosbank.forge.demo;
+
+import java.io.Serializable;
+
+public class HelloJBossWorld implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (name != null && !name.trim().isEmpty())
+			result += "name: " + name;
+		return result;
+	}
+}
